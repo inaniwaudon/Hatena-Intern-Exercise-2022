1c7df8cf30ae612cffa13fe47525f917e4b674c9
inaniwaudon <w.yuto0916@gmail.com>
2022-08-18 03:18:59 +0900

    implement fizzbuzz

--- typescript/src/fizzbuzz.ts
+++ typescript/src/fizzbuzz.ts
@@ -4,5 +4,18 @@ export function initializeFizzBuzz(
 ): void {
   console.debug("initializeFizzBuzz", list, button);
 
-  // Implement Me!
+  let no = 0;
+  const getFizzBuzz = () => {
+    ++no;
+    if (no % 3 === 0 || no % 5 === 0) {
+      return (no % 3 === 0 ? "Fizz" : "") + (no % 5 === 0 ? "Buzz" : "");
+    }
+    return no.toString();
+  };
+
+  button.addEventListener("click", () => {
+    const li = document.createElement("li");
+    li.innerHTML = getFizzBuzz();
+    list.appendChild(li);
+  });
 }

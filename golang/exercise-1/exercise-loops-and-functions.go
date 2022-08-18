785067decbcdacc36a8f0cd8fe4ab7fd2f757d96
inaniwaudon <w.yuto0916@gmail.com>
2022-08-18 22:47:42 +0900

    exercise 1 and 2

--- golang/exercise-1/exercise-loops-and-functions.go
+++ golang/exercise-1/exercise-loops-and-functions.go
@@ -5,6 +5,11 @@ import (
 )
 
 func Sqrt(x float64) float64 {
+	z := 1.0
+	for i := 0; i < 10; i++ {
+		z -= (z * z - x) / (2 * z)
+	}
+	return z
 }
 
 func main() {
--- golang/exercise-2/exercise-slices.go
+++ golang/exercise-2/exercise-slices.go
@@ -3,6 +3,14 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
+  picture := make([][]uint8, dy)
+  for y := 0; y < dy; y++ {
+		picture[y] = make([]uint8, dx)
+		for x := 0; x < dx; x++ {
+			picture[y][x] = uint8(x ^ y)
+		}
+  }
+  return picture
 }
 
 func main() {

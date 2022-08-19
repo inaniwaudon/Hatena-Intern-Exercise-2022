b57f8724b92e0efb63596c31dff2dd1b06cf6882
inaniwaudon <w.yuto0916@gmail.com>
2022-08-19 23:38:53 +0900

    exercise 7, 8, 9

--- golang/exercise-8/exercise-rot-reader.go
+++ golang/exercise-8/exercise-rot-reader.go
@@ -10,6 +10,22 @@ type rot13Reader struct {
 	r io.Reader
 }
 
+func (reader rot13Reader) Read(p []byte) (int, error) {
+	l, ok := reader.r.Read(p)
+	if ok == nil {
+		for i := 0; i < l; i++ {
+			temp := p[i]
+			if ('A' <= temp && temp <= 'Z') || ('a' <= temp && temp <= 'z') {
+				p[i] += 13
+			}
+			if ('Z'-13 < temp && temp <= 'Z') || 'z'-13 < temp {
+				p[i] -= 26
+			}
+		}
+	}
+	return l, ok
+}
+
 func main() {
 	s := strings.NewReader("Lbh penpxrq gur pbqr!")
 	r := rot13Reader{s}
--- golang/exercise-9/exercise-images.go
+++ golang/exercise-9/exercise-images.go
@@ -1,8 +1,26 @@
 package main
 
-import "golang.org/x/tour/pic"
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+)
 
 type Image struct{}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+	rect := image.Rect(0, 0, 100, 100)
+	return rect
+}
+
+func (i Image) At(x int, y int) color.Color {
+	v := uint8((x - y) ^ (x + y))
+	return color.RGBA{v, v, 255, 255}
+}
 
 func main() {
 	m := Image{}
